19e48b446ce1433d1968e6a5d853d207063864cb
Cali-Yare <shashamaxamed@gmail.com>
2026-09-25 09:01:37 +0300

    Addign Assigments

--- Assigment1/AssignmentOne.php
+++ Assigment1/AssignmentOne.php
@@ -7,36 +7,49 @@
 </head>
 <body>
     <?php
-$num1 = 25;
-$num2 = 10;
-$num3 = 18;
+// $num1 = 25;
+// $num2 = 10;
+// $num3 = 18;
 
 
-if ($num1 >= $num2 && $num1 >= $num3) {
-    $greatest = $num1;
-} elseif ($num2 >= $num1 && $num2 >= $num3) {
-    $greatest = $num2;
-} else {
-    $greatest = $num3;
-}
+// if ($num1 >= $num2 && $num1 >= $num3) {
+//     $greatest = $num1;
+// } elseif ($num2 >= $num1 && $num2 >= $num3) {
+//     $greatest = $num2;
+// } else {
+//     $greatest = $num3;
+// }
 
-echo "<br>";
+// echo "<br>";
 
-$number1 = 25;
-$number2 = 10;
-$number3 = 18;
+// $number1 = 25;
+// $number2 = 10;
+// $number3 = 18;
 
 
-if ($number1 <= $number2 && $number1 <= $number3) {
-    $lowest = $number1;
-} elseif ($number2 <= $number1 && $number2 <= $number3) {
-    $lowest = $number2;
-} else {
-    $lowest = $number3;
-}
- echo "the gretest number is $greatest";
- echo "<br>";
- echo "the lowest number is $lowest";
+// if ($number1 <= $number2 && $number1 <= $number3) {
+//     $lowest = $number1;
+// } elseif ($number2 <= $number1 && $number2 <= $number3) {
+//     $lowest = $number2;
+// } else {
+//     $lowest = $number3;
+// }
+//  echo "the gretest number is $greatest";
+//  echo "<br>";
+//  echo "the lowest number is $lowest";
+
+ $num =9;
+ if($num % 2==0 && $num % 3==0){
+    echo"it can divible by 2 and 3";
+
+ }elseif($num % 2==0){
+    echo"can be divisible by 2";
+
+ }elseif($num % 3==0){
+    echo"can be divisble by 3";
+ }else{
+    echo"it can nit be divisible";
+ }
     ?>
 
 
